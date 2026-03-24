6a2fc2cda9f64e8a96aad44ff8ed810da7cf4ee8
Otavio Cartaxo <oacartaxo@gmail.com>
2026-03-24 17:23:41 -0300

    avl implementation

---
+++ avl.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+struct AVLNode {
+    AVLNode *parent = nullptr;
+    AVLNode *left = nullptr;
+    AVLNode *right = nullptr;
+    uint32_t height = 0;
+    uint32_t cnt = 0;
+};
+
+inline void avl_init(AVLNode *node) {
+    node->left = node->right = node->parent = nullptr;
+    node->height = 1;
+    node->cnt = 1;
+}
+
+// helpers
+inline uint32_t avl_height(AVLNode *node) { return node ? node->height : 0; }
+inline uint32_t avl_cnt(AVLNode *node) { return node ? node->cnt : 0; }
+
+
+// API
+AVLNode *avl_fix(AVLNode *node);
+AVLNode *avl_del(AVLNode *node);
